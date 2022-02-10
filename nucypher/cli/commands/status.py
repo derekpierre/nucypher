64217a9b1cc3960d4a2bb44c1f33456692483572
KPrasch <kieranprasch@gmail.com>
2022-02-10 09:46:24 -0800

    Apply suggestions from code review
    Co-authored-by: Derek Pierre <derek.pierre@gmail.com>

--- nucypher/cli/commands/status.py
+++ nucypher/cli/commands/status.py
@@ -210,7 +210,7 @@ def events(general_config, registry_options, contract_name, from_block, to_block
                 contract_name=contract_name,
                 contract_version=contract_version,
                 proxy_name='Dispatcher',
-                use_proxy_address=True,
+                use_proxy_address=True
                )
             agent = EthereumContractAgent(contract=versioned_contract)
             agent.contract_name = contract_name
