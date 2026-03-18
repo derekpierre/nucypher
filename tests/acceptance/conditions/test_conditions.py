2d66a87b0d127fe6116ea98ec1caea124a220cd0
David Núñez <david@nucypher.com>
2026-03-18 19:53:21 +0100

    Adapt code and tests for changes in getActiveStakingProviders
    Only change is that duration parameter was removed, but there's a bit of cascade effect in several places

--- tests/acceptance/conditions/test_conditions.py
+++ tests/acceptance/conditions/test_conditions.py
@@ -780,7 +780,7 @@ def test_contract_condition_using_overloaded_function(
     (
         total_staked,
         providers,
-    ) = taco_child_application_agent._get_active_staking_providers_raw(0, 10, 0)
+    ) = taco_child_application_agent._get_active_staking_providers_raw(0, 10)
     expected_result = [
         total_staked,
         [
@@ -995,7 +995,7 @@ def test_big_int_string_handling(
     (
         total_staked,
         providers,
-    ) = taco_child_application_agent._get_active_staking_providers_raw(0, 10, 0)
+    ) = taco_child_application_agent._get_active_staking_providers_raw(0, 10)
     expected_result = [
         total_staked,
         [
